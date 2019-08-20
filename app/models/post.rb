746a7f2c533773c31dc7f7246ee80dd59bb23e9e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-08-19 17:15:54 -0700

    Adding validations to post and user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  validates :content, presence: :true, lenght: {maximum: 500}
 end
